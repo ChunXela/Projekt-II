5560d38aac23ce9666b5fbd4435367ed8d3049f1
ChunXela <johansson.axel@protonmail.com>
2022-02-21 19:21:01 +0100

    cry

--- Index.js
+++ Index.js
@@ -1,19 +1,17 @@
 const BtnAdd = document.querySelector(".makePost");
-const DivContainer = document.getElementById("postArea")
-const BtnAdd2 = document.querySelector(".postArea");
-const NavContainer = document.getElementById("box")
+const DivContainer = document.getElementById("postArea");
+const NavContainer = document.getElementById("box");
 
-BtnAdd2.addEventListener("click",NewNav);
 BtnAdd.addEventListener("click",AddNew);
 
 
 function AddNew(){
     const newDiv = document.createElement("div");
     console.log("Div");
-    newDiv.classList.add("postArea")
+    newDiv.classList.add("postArea");
     DivContainer.appendChild(newDiv);
 }
-
+/*
 function NewNav(){
     const newNav = document.createElement("nav");
     console.log("nav");
@@ -21,3 +19,4 @@ function NewNav(){
     NavContainer.appendChild(newNav);
 
 }
+*/
