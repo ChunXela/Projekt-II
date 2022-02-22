fc4037629fab38159013dc5ceac005d6dbabd5ac
ChunXela <Johansson.axel@protonmail.com>
2022-02-22 13:02:10 +0100

    ITS ALIVE, IT IS ALIVE

--- Index.js
+++ Index.js
@@ -1,22 +1,26 @@
 const BtnAdd = document.querySelector(".makePost");
-const DivContainer = document.getElementById("postArea");
-const NavContainer = document.getElementById("box");
+const DivContainer = document.querySelector(".homePage");
+const NavContainer = document.querySelector(".postArea");
 
 BtnAdd.addEventListener("click",AddNew);
 
 
 function AddNew(){
-    const newDiv = document.createElement("div");
-    console.log("Div");
+    let newDiv = document.createElement("div");
     newDiv.classList.add("postArea");
-    DivContainer.appendChild("newDiv");
-}
-/*
-function NewNav(){
-    const newNav = document.createElement("nav");
-    console.log("nav");
-    newNav.classList.add("box")
-    NavContainer.appendChild(newNav);
+    let newA = document.createElement("a")
+    newDiv.append(newA);
+    let newNav = document.createElement("nav");
+    newNav.classList.add("box");
+    newA.append(newNav);
+    let newName = document.createElement("div");
+    newName.classList.add("nameofsomething");
+    newNav.append(newName);
+    let name = document.createElement("h3");
+    name.classList.add("nameofPost");
+    newName.append(name);
+    document.getElementsByClassName("nameofPost").innerHTML = "name of something";
+
+    document.querySelector("main").prepend(newDiv);
 
 }
-*/
