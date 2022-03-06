d7be69b0a57d20eaaa749331038de79b163c0aa2
ChunXela <johansson.axel@protonmail.com>
2022-03-06 17:31:24 +0100

    Formating and pascal case

--- Index.js
+++ Index.js
@@ -2,51 +2,50 @@ const BtnAdd = document.querySelector(".makePost");
 const DivContainer = document.querySelector(".homePage");
 const NavContainer = document.querySelector(".postArea");
 
-BtnAdd.addEventListener("click",AddNew);
+BtnAdd.addEventListener("click", AddNew);
 
+function AddNew() {
+  let newDiv = document.createElement("div");
+  newDiv.classList.add("postArea");
 
-function AddNew(){
-    let newDiv = document.createElement("div");
-    newDiv.classList.add("postArea");
+  let newA = document.createElement("a");
+  newDiv.append(newA);
 
-    let newA = document.createElement("a")
-    newDiv.append(newA);
-    
-    let newNav = document.createElement("nav");
-    newNav.classList.add("box");
-    newA.append(newNav);
+  let newNav = document.createElement("nav");
+  newNav.classList.add("box");
+  newA.append(newNav);
 
-    let newName = document.createElement("div");
-    newName.classList.add("nameofsomething");
-    newNav.append(newName);
-    
-    let name = document.createElement("h3");
-    name.classList.add("nameofPost");
-    let nameText = document.createTextNode("name of something");
-    name.append(nameText);
-    newName.append(name);
+  let newName = document.createElement("div");
+  newName.classList.add("nameOfSomething");
+  newNav.append(newName);
 
-    document.getElementsByClassName("nameofPost").innerHTML = "name of something";
-    let img = document.createElement("img");
-    img.classList.add("imageofsomething");
-    img.setAttribute("src", "2.jpg")
-    newNav.append(img);
+  let name = document.createElement("h3");
+  name.classList.add("nameOfPost");
+  let nameText = document.createTextNode("name of something");
+  name.append(nameText);
+  newName.append(name);
 
-    let reactionbox = document.createElement("div");
-    reactionbox.classList.add("infoofsomething");
-    newNav.append(reactionbox);
+  document.getElementsByClassName("nameOfPost").innerHTML = "name of something";
+  let img = document.createElement("img");
+  img.classList.add("imageOfSomething");
+  img.setAttribute("src", "2.jpg");
+  newNav.append(img);
 
-    let like = document.createElement("nav")
-    like.classList.add("likebutton");
-    let likeText = document.createTextNode("Like");
-    like.append(likeText);
-    reactionbox.append(like); 
+  let reactionBox = document.createElement("div");
+  reactionBox.classList.add("infoOfSomething");
+  newNav.append(reactionBox);
 
-    let favorite = document.createElement("nav");
-    favorite.classList.add("favoritebutton");
-    let favoriteText = document.createTextNode("favorite");
-    favorite.append(favoriteText);
-    reactionbox.append(favorite);
+  let like = document.createElement("nav");
+  like.classList.add("likeButton");
+  let likeText = document.createTextNode("Like");
+  like.append(likeText);
+  reactionBox.append(like);
 
-    document.querySelector("main").prepend(newDiv);
+  let favorite = document.createElement("nav");
+  favorite.classList.add("favoriteButton");
+  let favoriteText = document.createTextNode("favorite");
+  favorite.append(favoriteText);
+  reactionBox.append(favorite);
+
+  document.querySelector("main").prepend(newDiv);
 }
