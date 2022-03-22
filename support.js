aa457701c815751d50f57fa67780b52f5a8ed9e9
ChunXela <Johansson.axel@protonmail.com>
2022-03-22 15:15:02 +0100

    mobile fixes

--- support.js
+++ support.js
@@ -5,10 +5,10 @@ for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var questionsAndAnswers = this.nextElementSibling;
-    if (questionsAndAnswers.style.display === "block") {
+    if (questionsAndAnswers.style.display === "flex") {
       questionsAndAnswers.style.display = "none";
     } else {
-      questionsAndAnswers.style.display = "block";
+      questionsAndAnswers.style.display = "flex";
     }
   });
 }
