69644cd49ceab8acd77960e65bbc1c76499949f3
ChunXela <Johansson.axel@protonmail.com>
2022-03-22 15:53:22 +0100

    minor support page  style changes

--- support.js
+++ support.js
@@ -1,14 +1,14 @@
-var coll = document.getElementsByClassName("clickToExpand");
+var coll = document.getElementsByClassName("expandDiv");
 var i;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var questionsAndAnswers = this.nextElementSibling;
-    if (questionsAndAnswers.style.display === "flex") {
+    if (questionsAndAnswers.style.display === "block") {
       questionsAndAnswers.style.display = "none";
     } else {
-      questionsAndAnswers.style.display = "flex";
+      questionsAndAnswers.style.display = "block";
     }
   });
 }
